d87fe63f7ac7a10ad211b2f85dcafb3b135db84d
TPLV <m4xlion@yandex.ru>
2023-04-19 17:07:44 +0300

    ~

--- server.py
+++ server.py
@@ -94,7 +94,7 @@ def handle_dialog(req, res):
     ]:
         # Пользователь согласился, прощаемся.
         res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
-        res['response']['end_session'] = True
+        res['response']['end_session'] = False
         return
 
     # Если нет, то убеждаем его купить слона!
